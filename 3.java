0d3bb68413b2eaae8a40294945b94fcbe94707e2
ZnS-20 <mohda698@gmail.com>
2018-03-13 16:33:57 +0530

    Just testing git from sublime

--- 3.java
+++ 3.java
@@ -1,6 +1,7 @@
 /*Problem 3
  The prime factors of 13195 are 5,7,13 and 29.
  What is the largest prime factor of the number 600851475143 ?
+ Testing gist
  */
 
 class euler3
@@ -9,15 +10,20 @@ class euler3
 	{
 	 double n = 600851475143.00; //initializing the value of n.
 	 int i;
-	  for (i=2;i<=n;i++)
+	 double m=n/2;
+	  for (i=2;i<=m;++i)
 	  {
-	    if(n%i==0 && i%2!=0 && i%3!=0) //Condition to calculate prime factor.
+	    while(n%i==0)
 	   {
 	       n = n/i;
-	      if (n == 1)
-		   break;  //break the loop.
+	       if (n == 1)
+	       {
+	           System.out.println("Largest Prime Factors = "+i);
+		   break;//break the while loop.
+	       }
 	   }
+	   if(n==1)
+	   break;//break the for loop.
       }
-        System.out.println("Largest Prime Factors = "+i);
     }
 }
